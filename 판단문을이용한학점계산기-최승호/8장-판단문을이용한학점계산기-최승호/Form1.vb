07e7c9a0bc643c53e54abf0825f1d58d8792f082
choiseungho <seunghodan1@gmail.com>
2022-05-15 19:18:33 +0900

    1week1commit_복귀

---
+++ 판단문을이용한학점계산기-최승호/8장-판단문을이용한학점계산기-최승호/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ijumsu As Integer : Dim shakjum As String
+        If TextBox1.Text = "" Then
+            MessageBox.Show("점수를 입력하세요", "경고 1")
+            TextBox1.Focus()
+            Exit Sub
+
+        End If
+        ijumsu = Val(TextBox1.Text)
+        If ijumsu > 100 Or ijumsu < 0 Then
+            MessageBox.Show("0부터 100사이의 값을 입력하세요", "경고 2")
+            TextBox1.Focus() : Exit Sub
+        End If
+        If ijumsu >= 96 And ijumsu <= 100 Then
+            shakjum = "A+"
+        ElseIf ijumsu >= 91 Then
+            shakjum = "A0"
+        ElseIf ijumsu >= 86 Then
+            shakjum = "B+"
+        ElseIf ijumsu >= 81 Then
+            shakjum = "B0"
+        ElseIf ijumsu >= 76 Then
+            shakjum = "C+"
+        ElseIf ijumsu >= 71 Then
+            shakjum = "C0"
+        ElseIf ijumsu >= 66 Then
+            shakjum = "D+"
+        ElseIf ijumsu >= 60 Then
+            shakjum = "D0"
+        Else
+            shakjum = "f:재수강 당첨"
+        End If
+        TextBox2.Text = shakjum '변수에 담긴 문자를 textbox2에 출력
+    End Sub
+End Class
---
+++ 판단문을이용한학점계산기-최승호/8장-판단문을이용한학점계산기-최승호/Form4.vb
@@ -0,0 +1,74 @@
+﻿Public Class Form4
+    Dim ttextbox_input(9) As TextBox
+    Dim ttextbox_output(9) As TextBox
+    Private Sub Form4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ttextbox_input(0) = TextBox1
+        ttextbox_input(1) = TextBox2
+        ttextbox_input(2) = TextBox3
+        ttextbox_input(3) = TextBox4
+        ttextbox_input(4) = TextBox5
+        ttextbox_input(5) = TextBox6
+        ttextbox_input(6) = TextBox7
+        ttextbox_input(7) = TextBox8
+        ttextbox_input(8) = TextBox9
+
+        ttextbox_output(0) = TextBox10
+        ttextbox_output(1) = TextBox11
+        ttextbox_output(2) = TextBox12
+        ttextbox_output(3) = TextBox13
+        ttextbox_output(4) = TextBox14
+        ttextbox_output(5) = TextBox15
+        ttextbox_output(6) = TextBox16
+        ttextbox_output(7) = TextBox17
+        ttextbox_output(8) = TextBox18
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For i = 0 To 8
+            If ttextbox_input(i).Text = "" Then
+                MessageBox.Show("textbox" & i + 1 & ".text에 점수를 입력하세요", "경고 1-" & i + 1)
+            End If
+        Next
+        Dim ijumsu(9) As Integer
+        Dim shakjum(9) As String
+
+        For i = 0 To 8
+            ijumsu(i) = Val(ttextbox_input(i).text)
+        Next
+
+        For i = 0 To 8
+            If ijumsu(i) > 100 Or ijumsu(i) < 0 Then
+                MessageBox.Show("textbox" & i + 1 & "에 0부터 100사이의 값을 입력하세요", "경고 2-" & i + 1)
+                ttextbox_input(i).Focus()
+                Exit Sub
+            End If
+        Next
+
+        For i = 0 To 8
+            Select Case ijumsu(i)
+                Case Is >= 96
+                    shakjum(i) = "A+"
+                Case Is >= 91
+                    shakjum(i) = "A0"
+                Case Is >= 86
+                    shakjum(i) = "B+"
+                Case Is >= 81
+                    shakjum(i) = "B0"
+                Case Is >= 76
+                    shakjum(i) = "C+"
+                Case Is >= 71
+                    shakjum(i) = "C0"
+                Case Is >= 66
+                    shakjum(i) = "D+"
+                Case Is >= 60
+                    shakjum(i) = "D0"
+                Case Else
+                    shakjum(i) = "F! 재수강 ㅠㅠ"
+            End Select
+        Next
+        For i = 0 To 8
+            ttextbox_output(i).Text = shakjum(i)
+
+        Next
+    End Sub
+End Class
